1144fa24bece46daa89b1e8c82e51a79d281e9a7
13484454324 <timo4396@163.com>
2023-02-09 10:03:07 +0800

    增加配置文件

--- src/main/java/com/example/yourproject/common/log/LogAspect.java
+++ src/main/java/com/example/yourproject/common/log/LogAspect.java
@@ -1,5 +1,7 @@
 package com.example.yourproject.common.log;
-//@Aspect
+
+import org.aspectj.lang.annotation.Aspect;
+
 /**
  * 功能描述:增加AOP切面
  * @param:
@@ -7,5 +9,6 @@ package com.example.yourproject.common.log;
  * @auther: LiuKun
  * @date: 2023-2-8 17:31
  */
+@Aspect
 public class LogAspect {
 }
